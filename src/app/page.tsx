4d163afdd22999f5bc8c6047113e730e5ff607ff
Mirza Shahbaz Ali Baig <mirzashahbazbaig724@gmail.com>
2025-12-28 23:02:12 +0500

    database migration and seeding completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+"use client"
 import Image from "next/image";
-
+import { useUser } from "@clerk/nextjs";
 export default function Home() {
+  const {user} = useUser();
+  const fullName = user?.fullName;
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black text-4xl text-amber-500">
-      Mirza Shahbaz Ali Baig is a very good boy
+      Mirza Shahbaz Ali Baig is a very Good Boy
+      <br />
+      <h1>{fullName}</h1>
     </div>
   );
 }
